667e893f38661a21ae6e2b7bd2ec1b2a055f84f0
ahmxdrif <78721827+ahmxdrif@users.noreply.github.com>
2024-03-19 09:42:51 +0800

    Add files via upload

---
+++ productconfigapi/src/main/java/com/example/productconfigapi/ProductconfigapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.productconfigapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductconfigapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductconfigapiApplication.class, args);
+	}
+
+}
---
+++ productconfigapi/src/test/java/com/example/productconfigapi/ProductconfigapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productconfigapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductconfigapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
